2a8a33b94a4fb08a5d5e3c2a53616067561778ab
Getachew Hundera <ghundera1@gmail.com>
2023-06-12 10:08:20 -0500

    added img, answered html questions

---
+++ assignment/helloWorld.js
@@ -0,0 +1,28 @@
+console.log("Hello World")
+
+tempRightNow=  
+
+let tempLater= '<p class="container">cool</p>'
+let game='zelda';
+let title= 'Totk'; 
+//let year = "2023"
+game= game + '' + title //creates a space for strings 
+console.log('game' , game); 
+
+let isCloudy = false;
+
+let toReplace= document.querySelector(#'fun2')
+// documnet refers to whole html documnet 
+// dom manipulaion. 
+//quesryselector goes through documnet and "finds" what you provided it. try to stick to use only ids for more specific items 
+// the value of fun2 is being assighned to toReplace
+// conditionals often use booleans to define a vlue. 
+
+toReplace.innerHTML = tempLater; 
+if (isCloudy === false){
+    console.log('clear skys'); 
+} else{
+    console.log('rainy day');
+}
+
+//=== equal to and type meaing that it evaluates how the stament is written like is it a string or number and it evelautes and takes into account for it all. 
